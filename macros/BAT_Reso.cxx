10ce2143dab61f25cc52d13cbbb708f7fcdc9153
tdixon <toby.dixon@universite-paris-saclay.fr>
2023-05-11 19:38:16 +0200

    adding command line options to reso macro

--- macros/BAT_Reso.cxx
+++ macros/BAT_Reso.cxx
@@ -1,6 +1,14 @@
 #include "TString.h"
 #include "TFile.h"
 #include "TH1D.h"
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <getopt.h>
+#include <utility>
+#include "TSpectrum.h"
+#include <fstream>
 #include "src/BatGraphFitter.h"
 #include "TFile.h"
 #include <BAT/BCLog.h>
@@ -106,134 +114,198 @@ std::vector<LineShapeResults> ReadInputText2Map(TString path)
 
   return input;
 }
-  
 
+void Usage()
+{
+  std::cout << std::endl << std::endl;
+  std::cout << "---------------------------------------------------------------------------------" << std::endl;
+  if( std::getenv("USER") != NULL )
+    std::cout << "Hi " << std::getenv("USER") <<"! The usage of this wonderful program is: " << std::endl;
+  else
+    std::cout << "Hi there! The usage of this wonderful program is:" << std::endl;
+  std::cout<<"./BAT_Reso"<<std::endl;
+  std::cout<<"options "<<std::endl;
+  std::cout<<"------------------------------------------------------------"<<std::endl;
+  std::cout<<"-i (or --input-path)        [directory for input txt] (default: inputs/reso/fitresults)"<<std::endl;
+  std::cout<<"-o (or --output-path)       [directory for output file (default: output/CUORE_reso/] "<<std::endl;
+  std::cout<<"-d (or --dataset)           [dataset number : default 3602] "<<std::endl;
+  std::cout<<"-r (or --reso-function)     [resolution function: default sqrt([0]*[0]+[1]*x)]"<<std::endl;
+  std::cout<<"-b (or --bias-function)     [bias function: default [0]*[0]+[1]*x+[2]*x*x]"<<std::endl;
+  std::cout<<"-l (or --label)             [label for outputs default "" ]"<<std::endl;
+  std::cout<<"-q (or --q-value)           [q-value to evaluate the function at default: 2527 keV]"<<std::endl;
+  std::cout<<"-h (or --help)              [this help]"<<std::endl;
+
+}
   
 int main(int argc, char **argv)
 {
 
   
-  // please replace with a cfg file
-  //std::map<TString,PeakInfo> peak_map;
-  TApplication *app  =new TApplication("app",&argc,argv);
-  gROOT->SetBatch(1);
-
-  TString reso_type="sqrt_pol1";
-
-  TString function_bias="[0]+[1]*x+[2]*x*x";
-  TString function_reso="[0]+[1]*x+[2]*x*x";
-  int NparReso=3;
-  if (reso_type=="pol1")
-    {
-      function_reso="[0]+[1]*x";
-      NparReso=2;
-    }
-  else if (reso_type=="sqrt_pol1")
-    {
-      function_reso="sqrt([0]*[0]+[1]*x)";
-      NparReso=2;
-    }
-  else if (reso_type=="sqrt_pol2")
-    {
-      function_reso="sqrt([0]*[0]+[1]*x+[2]*x*x)";
-    }
-
-  std::cout<<function_reso<<std::endl;
-  // READ THE DATA
-  // ----------------------------------------------------------
-  
-  // hardcoded should change !!!!
-  int ds=atoi(argv[1]);
-  double Qbb=2527;
+  int ds=3602;
+  TString bias_function="[0]*[0]+[1]*x+[2]*x*x";
+  TString reso_function="sqrt([0]*[0]+[1]*x)";
   TString path="inputs/reso/fitresults";
-  std::vector<LineShapeResults> input = ReadInputText2Map(Form("%s_ds%i.dat",path.Data(),ds));
+  TString out_path = Form("output/CUORE_reso/");
+  TString label="";
+  double Qbb=2527;
+  {
+  static struct option long_options[] = {
+					 { "input-path",        required_argument,  nullptr, 'i' },
+					 { "output-path",       required_argument,  nullptr,'o'},
+					 { "dataset",required_argument, nullptr,'d'},
+					 { "bias-function",      required_argument,nullptr,'b'  },
+					 { "reso-function",         required_argument,  nullptr,'r'},
+					 { "label",    required_argument, nullptr,'l'},
+					 { "q-value",           required_argument,  nullptr, 'q' },
+					 { "help",              no_argument,        nullptr,'h'},
+					 {nullptr, 0, nullptr, 0}
+  };
+  const char* const short_options = "i:o:d:r:b:l:q:h";
+  
+  int c;
+  
+  while ((c = getopt_long(argc, argv, short_options, long_options, nullptr)) != -1 ) {
+    switch (c) {
+    case 'i': {
+      path = optarg;
+      break;
+    }
 
+    case 'o': {
+      out_path = optarg;
+      break;
+    }
+    case 'l':{
+      label=optarg;
+      break;
+	}
+      
+    case 'r': {
+      reso_function = optarg;
+      break;
+    }
+      
+    case 'b': {
+      bias_function = optarg;
+      break;
+    }
 
-  // create graphs
-
-  TGraphErrors * gerror_bias = new TGraphErrors();
-  TGraphErrors * gerror_reso = new TGraphErrors();
-
-  for (int i=0;i<input.size();i++)
-    {
-      gerror_bias->SetPoint(i,input[i].EnergyNom,input[i].EnergyFit-input[i].EnergyNom);
-      gerror_bias->SetPointError(i,0,input[i].ErrorEnergyFit);
-      gerror_reso->SetPoint(i,input[i].EnergyNom,input[i].Scaling);
-      gerror_reso->SetPointError(i,0,input[i].ErrorScaling);
+      
+    case 'q': {
+      Qbb = atof(optarg);
+      break;
+    }
+    case'h': {
+      Usage();
+      return 0;
+    }
+    default: {
+      exit(1);
+    }
       
     }
-  gerror_bias->SetTitle(Form("Fit to energy bias for ds %i ; Energy Nominal [keV] ; Energy Fit - Energy Nominal [keV] ;",ds));
-  gerror_reso->SetTitle(Form("Fit to energy resolution scaling for ds %i ; Energy Nominal [keV] ; Scaling [] ;",ds));
+
+    
+      
+   } 
+  }
+
+    gROOT->SetBatch(1);
+    
+
+    // READ THE DATA
+    // ----------------------------------------------------------
+    
+    std::vector<LineShapeResults> input = ReadInputText2Map(Form("%s_ds%i.dat",path.Data(),ds));
+    
+
+    // create graphs
+    
+    TGraphErrors * gerror_bias = new TGraphErrors();
+    TGraphErrors * gerror_reso = new TGraphErrors();
+    
+    for (int i=0;i<input.size();i++)
+      {
+	gerror_bias->SetPoint(i,input[i].EnergyNom,input[i].EnergyFit-input[i].EnergyNom);
+	gerror_bias->SetPointError(i,0,input[i].ErrorEnergyFit);
+	gerror_reso->SetPoint(i,input[i].EnergyNom,input[i].Scaling);
+	gerror_reso->SetPointError(i,0,input[i].ErrorScaling);
+	
+      }
+    gerror_bias->SetTitle(Form("Fit to energy bias for ds %i ; Energy Nominal [keV] ; Energy Fit - Energy Nominal [keV] ;",ds));
+    gerror_reso->SetTitle(Form("Fit to energy resolution scaling for ds %i ; Energy Nominal [keV] ; Scaling [] ;",ds));
   
-  path = Form("output/CUORE_reso/ds%i/",ds);
+    
+    TCanvas *ce = new TCanvas();
 
-  TCanvas *ce = new TCanvas();
-
-
-  
-  // CREATE A FUNCTION TO FIT THE BIAS
-  //------------------------------------------
-  TF1 *fBias = new TF1("fBias","[0]+[1]*x+[2]*x*x",0,4000);
-
-  gerror_bias->Fit(fBias);
-  fBias->SetParLimits(0,fBias->GetParameters()[0]-5*fBias->GetParErrors()[0],fBias->GetParameters()[0]+5*fBias->GetParErrors()[0]);
-  fBias->SetParLimits(1,fBias->GetParameters()[1]-5*fBias->GetParErrors()[1],fBias->GetParameters()[1]+5*fBias->GetParErrors()[1]);
-  fBias->SetParLimits(2,fBias->GetParameters()[2]-5*fBias->GetParErrors()[2],fBias->GetParameters()[2]+5*fBias->GetParErrors()[2]);
-
-  
-  TF1 *fReso = new TF1("fReso",function_reso,0,4000);
-  gerror_reso->Fit(fReso);
-  
-  fReso->SetParLimits(0,fReso->GetParameters()[0]-5*fReso->GetParErrors()[0],fReso->GetParameters()[0]+5*fReso->GetParErrors()[0]);
-  fReso->SetParLimits(1,fReso->GetParameters()[1]-5*fReso->GetParErrors()[1],fReso->GetParameters()[1]+5*fReso->GetParErrors()[1]);
-  fReso->SetParLimits(2,fReso->GetParameters()[2]-5*fReso->GetParErrors()[2],fReso->GetParameters()[2]+5*fReso->GetParErrors()[2]);
-
-  // CREATE THE BAT fitter (bias)
-  // -----------------------------------------------
-  
-  BatGraphFitter *fitter_bias = new BatGraphFitter(gerror_bias,fBias);
-  fitter_bias->fModel->SetObsRange(-2,2);
-  fitter_bias->SetPrecison(4);
-  fitter_bias->SetQbb(Qbb);
-  fitter_bias->SetGraphMaxMin(20,-20);  
-  fitter_bias->Fit();
-
-  // important - 3rd parameter is hardcoded
-  TH1D* margdistro_bias = (TH1D*)fitter_bias->fModel->GetMarginalizedHistogram(3);
-  GetCI(margdistro_bias);
-
-  ce->Draw();
-  ce->Print(Form("%s/bias.pdf(",path.Data()),"pdf");
-  margdistro_bias->Draw();
-  ce->Draw();
-  ce->Print(Form("%s/bias.pdf)",path.Data()),"pdf");
-  fitter_bias->fModel->PrintAllMarginalized(Form("%s/bias_fit.pdf",path.Data()));
-  fitter_bias->fModel->WriteMarginalizedDistributions(Form("%s/output_bias.root",path.Data()), "RECREATE");
-
-
-  // SAME FOR RESO
-  // --------------------------------------------------------
 
   
-  BatGraphFitter *fitter_reso = new BatGraphFitter(gerror_reso,fReso);
-  fitter_bias->fModel->SetObsRange(0.8,1.2);
+    // CREATE A FUNCTION TO FIT THE BIAS
+    //------------------------------------------
+    TF1 *fBias = new TF1("fBias",bias_function,0,4000);
+    
+    gerror_bias->Fit(fBias);
+    fBias->SetParLimits(0,fBias->GetParameters()[0]-5*fBias->GetParErrors()[0],fBias->GetParameters()[0]+5*fBias->GetParErrors()[0]);
+    fBias->SetParLimits(1,fBias->GetParameters()[1]-5*fBias->GetParErrors()[1],fBias->GetParameters()[1]+5*fBias->GetParErrors()[1]);
+    fBias->SetParLimits(2,fBias->GetParameters()[2]-5*fBias->GetParErrors()[2],fBias->GetParameters()[2]+5*fBias->GetParErrors()[2]);
 
-  fitter_reso->SetPrecison(4);
-  fitter_reso->SetQbb(Qbb);
-  fitter_reso->SetGraphMaxMin(2,0);
-  fitter_reso->Fit();
+  
+    TF1 *fReso = new TF1("fReso",reso_function,0,4000);
+    gerror_reso->Fit(fReso);
+    
+    fReso->SetParLimits(0,fReso->GetParameters()[0]-5*fReso->GetParErrors()[0],fReso->GetParameters()[0]+5*fReso->GetParErrors()[0]);
+    fReso->SetParLimits(1,fReso->GetParameters()[1]-5*fReso->GetParErrors()[1],fReso->GetParameters()[1]+5*fReso->GetParErrors()[1]);
+    fReso->SetParLimits(2,fReso->GetParameters()[2]-5*fReso->GetParErrors()[2],fReso->GetParameters()[2]+5*fReso->GetParErrors()[2]);
 
-  TH1D* margdistro_reso = (TH1D*)fitter_reso->fModel->GetMarginalizedHistogram(NparReso);
-  GetCI(margdistro_reso);
+    // CREATE THE BAT fitter (bias)
+    // -----------------------------------------------
+  
+    BatGraphFitter *fitter_bias = new BatGraphFitter(gerror_bias,fBias);
+    fitter_bias->fModel->SetObsRange(-2,2);
+    fitter_bias->SetPrecison(4);
+    fitter_bias->SetQbb(Qbb);
+    fitter_bias->SetGraphMaxMin(20,-20);  
+    fitter_bias->Fit();
+    gerror_bias->SetTitle(Form("Fit to energy bias for ds %i ; Energy Nominal [keV] ; Energy Fit - Energy Nominal [keV] ;",ds));
 
-  // save
-  ce->Draw();
-  ce->Print(Form("%s/reso_%s.pdf(",path.Data(),reso_type.Data()),"pdf");
-  margdistro_reso->Draw();
-  ce->Draw();
-  ce->Print(Form("%s/reso_%s.pdf)",path.Data(),reso_type.Data()),"pdf");
-  fitter_reso->fModel->PrintAllMarginalized(Form("%s/reso_fit_%s.pdf",path.Data(),reso_type.Data()));
-  fitter_reso->fModel->WriteMarginalizedDistributions(Form("%s/output_reso_%s.root",path.Data(),reso_type.Data()), "RECREATE");
+
+    // important - 3rd parameter is hardcoded
+    TH1D* margdistro_bias = (TH1D*)fitter_bias->fModel->GetMarginalizedHistogram(fBias->GetNpar());
+    GetCI(margdistro_bias);
+    
+    ce->Draw();
+    ce->Print(Form("%s/bias_%s.pdf(",out_path.Data(),label.Data()),"pdf");
+    margdistro_bias->Draw();
+    ce->Draw();
+    ce->Print(Form("%s/bias_%s.pdf)",out_path.Data(),label.Data()),"pdf");
+    fitter_bias->fModel->PrintAllMarginalized(Form("%s/bias_fit_%s.pdf",out_path.Data(),label.Data()));
+    fitter_bias->fModel->WriteMarginalizedDistributions(Form("%s/output_bias_%s.root",out_path.Data(),label.Data()), "RECREATE");
+    
+
+    // SAME FOR RESO
+    // --------------------------------------------------------
+
+  
+    BatGraphFitter *fitter_reso = new BatGraphFitter(gerror_reso,fReso);
+    fitter_bias->fModel->SetObsRange(0.8,1.2);
+
+    fitter_reso->SetPrecison(4);
+    fitter_reso->SetQbb(Qbb);
+    fitter_reso->SetGraphMaxMin(2,0);
+    fitter_reso->Fit();
+    gerror_reso->SetTitle(Form("Fit to energy resolution scaling for ds %i ; Energy Nominal [keV] ; Scaling [] ;",ds));
+
+    TH1D* margdistro_reso = (TH1D*)fitter_reso->fModel->GetMarginalizedHistogram(fReso->GetNpar());
+    GetCI(margdistro_reso);
+
+    // save
+    ce->Draw();
+    ce->Print(Form("%s/reso_%s.pdf(",out_path.Data(),label.Data()),"pdf");
+    margdistro_reso->Draw();
+    ce->Draw();
+    ce->Print(Form("%s/reso_%s.pdf)",out_path.Data(),label.Data()),"pdf");
+    fitter_reso->fModel->PrintAllMarginalized(Form("%s/reso_fit_%s.pdf",out_path.Data(),label.Data()));
+    fitter_reso->fModel->WriteMarginalizedDistributions(Form("%s/output_reso_%s.root",out_path.Data(),label.Data()), "RECREATE");
 
   
 
