f5ebb96b8440ce56a306a0513115b5f05d77945b
Giovanni Benato <benatogiovanni@gmail.com>
2023-08-08 16:16:52 +0200

    Cosmetics

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-#g++ -std=c++17 `root-config --cflags --glibs` macros/BAT_Efficiency.cxx src/BatGraphFitter.cxx src/BAT_GraphFit.cxx -o bin/BAT_Efficiency `bat-config --libs --cflags`
+g++ -std=c++17 `root-config --cflags --glibs` macros/BAT_Efficiency.cxx src/BatGraphFitter.cxx src/BAT_GraphFit.cxx -o bin/BAT_Efficiency `bat-config --libs --cflags`
 g++ -std=c++17  macros/PrintEfficiencyValues.cxx -o bin/PrintEfficiencyValues `root-config --cflags --glibs`
 #g++ -std=c++17 `root-config --cflags --glibs` macros/CombineEfficiency.cxx src/BatGraphFitter.cxx src/BAT_GraphFit.cxx -o bin/CombineEfficiency `bat-config --libs --cflags`
 #g++ -std=c++17 `root-config --cflags --glibs` macros/BAT_Reso.cxx src/BatGraphFitter.cxx src/BAT_GraphFit.cxx -o bin/BAT_Reso `bat-config --libs --cflags`
