d218821743d5bde1fd7864c800b1732e32f30f65
Giovanni Benato <gbenato@berkeley.edu>
2023-08-12 21:47:07 +0200

    minor fixes

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
-#g++ -std=c++17 `root-config --cflags --glibs` macros/BAT_Efficiency.cxx src/BatGraphFitter.cxx src/BAT_GraphFit.cxx -o bin/BAT_Efficiency `bat-config --libs --cflags`
-#g++ -std=c++17  macros/PrintEfficiencyValues.cxx -o bin/PrintEfficiencyValues `root-config --cflags --glibs`
-g++ -std=c++17  macros/CombineEfficiency.cxx src/BatGraphFitter.cxx src/BAT_GraphFit.cxx -o bin/CombineEfficiency `bat-config --libs --cflags`
-#g++ -std=c++17 `root-config --cflags --glibs` macros/BAT_Reso.cxx src/BatGraphFitter.cxx src/BAT_GraphFit.cxx -o bin/BAT_Reso `bat-config --libs --cflags`
-#g++ -std=c++17  macros/CombineReso.cxx src/BatGraphFitter.cxx src/BAT_GraphFit.cxx -o bin/CombineReso `bat-config --libs --cflags`
+g++ -std=c++11 `root-config --cflags --glibs` macros/BAT_Efficiency.cxx src/BatGraphFitter.cxx src/BAT_GraphFit.cxx -o bin/BAT_Efficiency `bat-config --libs --cflags`
+g++ -std=c++11  macros/PrintEfficiencyValues.cxx -o bin/PrintEfficiencyValues `root-config --cflags --glibs`
+g++ -std=c++11  macros/CombineEfficiency.cxx src/BatGraphFitter.cxx src/BAT_GraphFit.cxx -o bin/CombineEfficiency `bat-config --libs --cflags`
+g++ -std=c++11 `root-config --cflags --glibs` macros/BAT_Reso.cxx src/BatGraphFitter.cxx src/BAT_GraphFit.cxx -o bin/BAT_Reso `bat-config --libs --cflags`
+g++ -std=c++11  macros/CombineReso.cxx src/BatGraphFitter.cxx src/BAT_GraphFit.cxx -o bin/CombineReso `bat-config --libs --cflags`
