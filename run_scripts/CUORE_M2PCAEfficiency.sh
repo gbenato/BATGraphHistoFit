ea3049f92861abb5c04fd4a9b40c515119aba07e
Giovanni Benato <gbenato@berkeley.edu>
2024-05-30 09:54:18 +0200

    Created scripts to run PCA efficiencies on M2

---
+++ run_scripts/CUORE_M2PCAEfficiency.sh
@@ -0,0 +1,103 @@
+/home/gbenato/Cuore/Analysis/BATGraphHistoFit/bin/BAT_Efficiency \
+    --input-path /home/gbenato/Cuore/Data/TAUP23/EfficiencyInput/Eff_ \
+    --output-path /home/gbenato/Cuore/Data/TAUP23/PCAEfficiency \
+    --cfg-path /home/gbenato/Cuore/Analysis/BATGraphHistoFit/cfg/EffPeaks.cfg \
+    --dataset 3816 \
+    --precision 3 \
+    --mode P &
+
+/home/gbenato/Cuore/Analysis/BATGraphHistoFit/bin/BAT_Efficiency \
+    --input-path /home/gbenato/Cuore/Data/TAUP23/EfficiencyInput/Eff_ \
+    --output-path /home/gbenato/Cuore/Data/TAUP23/PCAEfficiency \
+    --cfg-path /home/gbenato/Cuore/Analysis/BATGraphHistoFit/cfg/EffPeaks.cfg \
+    --dataset 3817 \
+    --precision 3 \
+    --mode P &
+
+/home/gbenato/Cuore/Analysis/BATGraphHistoFit/bin/BAT_Efficiency \
+    --input-path /home/gbenato/Cuore/Data/TAUP23/EfficiencyInput/Eff_ \
+    --output-path /home/gbenato/Cuore/Data/TAUP23/PCAEfficiency \
+    --cfg-path /home/gbenato/Cuore/Analysis/BATGraphHistoFit/cfg/EffPeaks.cfg \
+    --dataset 3818 \
+    --precision 3 \
+    --mode P &
+
+/home/gbenato/Cuore/Analysis/BATGraphHistoFit/bin/BAT_Efficiency \
+    --input-path /home/gbenato/Cuore/Data/TAUP23/EfficiencyInput/Eff_ \
+    --output-path /home/gbenato/Cuore/Data/TAUP23/PCAEfficiency \
+    --cfg-path /home/gbenato/Cuore/Analysis/BATGraphHistoFit/cfg/EffPeaks.cfg \
+    --dataset 3819 \
+    --precision 3 \
+    --mode P &
+
+/home/gbenato/Cuore/Analysis/BATGraphHistoFit/bin/BAT_Efficiency \
+    --input-path /home/gbenato/Cuore/Data/TAUP23/EfficiencyInput/Eff_ \
+    --output-path /home/gbenato/Cuore/Data/TAUP23/PCAEfficiency \
+    --cfg-path /home/gbenato/Cuore/Analysis/BATGraphHistoFit/cfg/EffPeaks.cfg \
+    --dataset 3820 \
+    --precision 3 \
+    --mode P &
+
+/home/gbenato/Cuore/Analysis/BATGraphHistoFit/bin/BAT_Efficiency \
+    --input-path /home/gbenato/Cuore/Data/TAUP23/EfficiencyInput/Eff_ \
+    --output-path /home/gbenato/Cuore/Data/TAUP23/PCAEfficiency \
+    --cfg-path /home/gbenato/Cuore/Analysis/BATGraphHistoFit/cfg/EffPeaks.cfg \
+    --dataset 3821 \
+    --precision 3 \
+    --mode P 
+
+/home/gbenato/Cuore/Analysis/BATGraphHistoFit/bin/BAT_Efficiency \
+    --input-path /home/gbenato/Cuore/Data/TAUP23/EfficiencyInput/Eff_ \
+    --output-path /home/gbenato/Cuore/Data/TAUP23/PCAEfficiency \
+    --cfg-path /home/gbenato/Cuore/Analysis/BATGraphHistoFit/cfg/EffPeaks.cfg \
+    --dataset 3822 \
+    --precision 3 \
+    --mode P &
+
+/home/gbenato/Cuore/Analysis/BATGraphHistoFit/bin/BAT_Efficiency \
+    --input-path /home/gbenato/Cuore/Data/TAUP23/EfficiencyInput/Eff_ \
+    --output-path /home/gbenato/Cuore/Data/TAUP23/PCAEfficiency \
+    --cfg-path /home/gbenato/Cuore/Analysis/BATGraphHistoFit/cfg/EffPeaks.cfg \
+    --dataset 3823 \
+    --precision 3 \
+    --mode P &
+
+/home/gbenato/Cuore/Analysis/BATGraphHistoFit/bin/BAT_Efficiency \
+    --input-path /home/gbenato/Cuore/Data/TAUP23/EfficiencyInput/Eff_ \
+    --output-path /home/gbenato/Cuore/Data/TAUP23/PCAEfficiency \
+    --cfg-path /home/gbenato/Cuore/Analysis/BATGraphHistoFit/cfg/EffPeaks.cfg \
+    --dataset 3824 \
+    --precision 3 \
+    --mode P &
+
+/home/gbenato/Cuore/Analysis/BATGraphHistoFit/bin/BAT_Efficiency \
+    --input-path /home/gbenato/Cuore/Data/TAUP23/EfficiencyInput/Eff_ \
+    --output-path /home/gbenato/Cuore/Data/TAUP23/PCAEfficiency \
+    --cfg-path /home/gbenato/Cuore/Analysis/BATGraphHistoFit/cfg/EffPeaks.cfg \
+    --dataset 3825 \
+    --precision 3 \
+    --mode P &
+
+/home/gbenato/Cuore/Analysis/BATGraphHistoFit/bin/BAT_Efficiency \
+    --input-path /home/gbenato/Cuore/Data/TAUP23/EfficiencyInput/Eff_ \
+    --output-path /home/gbenato/Cuore/Data/TAUP23/PCAEfficiency \
+    --cfg-path /home/gbenato/Cuore/Analysis/BATGraphHistoFit/cfg/EffPeaks.cfg \
+    --dataset 3826 \
+    --precision 3 \
+    --mode P &
+
+/home/gbenato/Cuore/Analysis/BATGraphHistoFit/bin/BAT_Efficiency \
+    --input-path /home/gbenato/Cuore/Data/TAUP23/EfficiencyInput/Eff_ \
+    --output-path /home/gbenato/Cuore/Data/TAUP23/PCAEfficiency \
+    --cfg-path /home/gbenato/Cuore/Analysis/BATGraphHistoFit/cfg/EffPeaks.cfg \
+    --dataset 3827 \
+    --precision 3 \
+    --mode P &
+
+/home/gbenato/Cuore/Analysis/BATGraphHistoFit/bin/BAT_Efficiency \
+    --input-path /home/gbenato/Cuore/Data/TAUP23/EfficiencyInput/Eff_ \
+    --output-path /home/gbenato/Cuore/Data/TAUP23/PCAEfficiency \
+    --cfg-path /home/gbenato/Cuore/Analysis/BATGraphHistoFit/cfg/EffPeaks.cfg \
+    --dataset 3828 \
+    --precision 3 \
+    --mode P
